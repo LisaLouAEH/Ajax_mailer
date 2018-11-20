44206676aebe8a29facfb239c43b2cb834defa75
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-11-20 19:52:32 +0100

    add script carrousel + main.scss dans app/asset

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
 
 //test de liaison entre l'app et le script:
@@ -23,14 +22,16 @@
 });*/
 
 // script de tabulation:
+
 $(document).ready(function() {
     $(".modal-signup").hide();
     $(".modal-login").hide();   
     $("ul > .card").hide();
     $(".dropdown > ul").hide();
+
     $(".container > ul > li").css("border", "solid green 0.3px");
     $(".container > ul > li").click(function(){
-        //$(".container > ul > li").css("background-color", "red");
+    //$(".container > ul > li").css("background-color", "red");
         
         if( $(this).next().is(":hidden") ) {
             $("ul > .card").hide(200);
@@ -40,7 +41,7 @@ $(document).ready(function() {
             $("ul > .card").hide(200);
         }  
         });
-// dropdown du avatar
+    // dropdown du avatar
 
     $(".rounded-circle").click(function() {
         if( $(this).next().is(":hidden")) {
@@ -51,6 +52,7 @@ $(document).ready(function() {
     });
     $(".dropdown > ul > li").css("list-style-type", "none");
 
+    // modal login 
     $(".login").click(function() {
         if( $(".modal-login").is(":hidden")) {
             $(".modal-login").show();
@@ -58,7 +60,7 @@ $(document).ready(function() {
              $(".modal-login").hide();
         };
     });
-
+    // modal signup
     $(".signup").click(function() {
         if( $(".modal-signup").is(":hidden")) {
             $(".modal-signup").show();
@@ -66,9 +68,61 @@ $(document).ready(function() {
              $(".modal-signup").hide();
         };    
     });
+
+    // section carrousel
+    var $carrousel = $('#carrousel'), // on cible le bloc du carrousel
+
+    $img = $('#carrousel img'), // on cible les images contenues dans le carrousel
+
+    indexImg = $img.length - 1, // on définit l'index du dernier élément
+
+    i = 0, // on initialise un compteur
+
+    $currentImg = $img.eq(i); // enfin, on cible l'image courante, qui possède l'index i (0 pour l'instant)
+
+        // cette partie determine les propriétés d'affichage par défaults
+    $img.css('display', 'none'); // on cache les images
+
+    $currentImg.css('display', 'block'); // on affiche seulement l'image courante
+    $carrousel.append('<div class="controls"> <button class="prev">○</button> <button class="next">○</button> </div>');
+
+    $('.next').click(function(){ // image suivante
+        i++;
+        if( i <= indexImg ){
+            $img.css('display', 'none'); // on cache les images
+            $currentImg = $img.eq(i); // position 'i'
+            $currentImg.css('display', 'block'); // puis on l'affiche
+        }
+        else{
+            i = indexImg;
+        }
+    });
+    $('.prev').click(function(){ // image précédente
+        i--;
+        if(i >= 0){
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);// position 'i'
+            $currentImg.css('display', 'block');
+        }
+        else{
+            i = 0;
+        }
+    });
+    // fonction slider d'image :
+    function slideImg(){
+        setTimeout(function(){ // on utilise une fonction anonyme
+            if(i < indexImg){ // si le compteur est inférieur au dernier index
+            i++; // on l'incrémente
+        }
+        else{ // sinon, on le remet à 0 (première image)
+            i = 0;
+        }
+        $img.css('display', 'none');
+        $currentImg = $img.eq(i);
+        $currentImg.css('display', 'block');
+        slideImg(); // relancer la fonction à la fin
+        }, 5000); // timer en millisecond (5sec)
+    }
+    slideImg();
 });
-
-
-
-
 //$(".cat__title").find("ul")
